b7a19345510a9652f93aa52e4920038716ed5f4c
NGHIADAM\PC <valkyrie1127@gmail.com>
2017-04-12 17:32:19 +0700

    Initial GameWindow Commit

---
+++ src/GameWindow.java
@@ -0,0 +1,49 @@
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class GameWindow extends Frame{
+    public GameWindow(){
+        setVisible(true);
+        setSize(600,800);
+
+        //add Listener
+        addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
+        
+    }
+}
